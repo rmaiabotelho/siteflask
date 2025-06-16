a34d0cb8561ca700f762cda75fb022861c0bef86
Rodrigo <rmaiabotelho@gmail.com>
2025-06-16 16:51:44 -0300

    Criei os arquivos contato.html e home.html

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
+# Serve para iniciar o flask
 app = Flask(__name__)
 
+# Rota para estipular em qual link a página estará disponível
 @app.route("/")
-def hello_world():
-    return "Olá, mundo!"
+# Função para regular o funcionamento da página, o que será exibido e como
+def home():
+    return render_template('home.html')
 
+@app.route('/contato')
+def contato():
+    return render_template('contato.html')
+
+# Para rodar a página somente pelo arquivo e não por importação
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True) # debug=True faz com que o site seja atualizado conforme as alterações são feitas
